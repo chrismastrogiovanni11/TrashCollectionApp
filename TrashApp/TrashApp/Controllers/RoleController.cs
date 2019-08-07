0d5ee7c4f5f389bb8da2428820ded8dc0b046196
chrismastrogiovanni11 <chrismaastro@gmail.com>
2019-08-07 16:19:46 -0500

    more of the roles done

---
+++ TrashApp/TrashApp/Controllers/RoleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashApp.Models;
+
+namespace TrashApp.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+        // GET: Role
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+
+
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+    }
+}
