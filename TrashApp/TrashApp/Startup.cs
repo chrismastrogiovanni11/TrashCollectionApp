f4ca646332f0442033496d3f1a904790c38f7b3d
chrismastrogiovanni11 <chrismaastro@gmail.com>
2019-08-07 16:04:28 -0500

    almost done with roles, took a little to figure out and understand but getting hang of it

---
+++ TrashApp/TrashApp/Startup.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using TrashApp.Models;
+
+[assembly: OwinStartupAttribute(typeof(TrashApp.Startup))]
+namespace TrashApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+
+
+                var user = new ApplicationUser();
+                user.UserName = "Christopher";
+                user.Email = "chrismastro@gmail.com";
+
+                string userPWD = "C@243208";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+
+
+            }
+
+               
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+
+
+            }
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+
+            }
+
+        }
+
+    }    
+        
+}
