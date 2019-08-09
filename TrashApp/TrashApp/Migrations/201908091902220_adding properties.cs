bb4dfb10955373d5cb7217579e93c8aa75821a31
chrismastrogiovanni11 <chrismaastro@gmail.com>
2019-08-09 15:13:27 -0500

    figured out view page problem

---
+++ TrashApp/TrashApp/Migrations/201908091902220_adding properties.cs
@@ -0,0 +1,16 @@
+namespace TrashApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addingproperties : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
